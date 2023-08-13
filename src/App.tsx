63c48c8dce0dc48a3217689854429c660da536b3
GmalFari <gmalfari926@gmail.com>
2023-08-13 16:03:30 +0300

    add this project to make  validation form with zod

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import Form from "./Form";
+
+function App() {
+  return (
+    <div className="flex mt-20 text-center justify-center w-full">
+      <div className="flext justify-center text-center w-3/4 my">
+        <div className="flext justify-center">
+          <h3 className=" font-bold text-3xl mb-3 text-lg ">
+            Create New Account
+          </h3>
+          <Form />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
